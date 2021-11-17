15624a88cccfadca0bbc5cfd5f072a4d51b71251
Javier Soto <javislop@gmail.com>
2021-11-17 00:18:20 -0500

    indentations

--- SocialMedia.Core/Entities/Comment.cs
+++ SocialMedia.Core/Entities/Comment.cs
@@ -4,16 +4,16 @@
 
 namespace SocialMedia.Core.Entities
 {
-  public partial class Comment
-  {
-    public int CommentId { get; set; }
-    public int PostId { get; set; }
-    public int UserId { get; set; }
-    public string Description { get; set; }
-    public DateTime Date { get; set; }
-    public bool IsActive { get; set; }
+    public partial class Comment
+    {
+        public int CommentId { get; set; }
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsActive { get; set; }
 
-    public virtual Post Post { get; set; }
-    public virtual User User { get; set; }
-  }
+        public virtual Post Post { get; set; }
+        public virtual User User { get; set; }
+    }
 }
--- SocialMedia.Core/Entities/User.cs
+++ SocialMedia.Core/Entities/User.cs
@@ -5,23 +5,23 @@ using System.Collections.Generic;
 
 namespace SocialMedia.Core.Entities
 {
-  public partial class User
-  {
-    public User()
+    public partial class User
     {
-      Comments = new HashSet<Comment>();
-      Posts = new HashSet<Post>();
+        public User()
+        {
+          Comments = new HashSet<Comment>();
+          Posts = new HashSet<Post>();
+        }
+
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsActive { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+        public virtual ICollection<Post> Posts { get; set; }
     }
-
-    public int UserId { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public DateTime DateOfBirth { get; set; }
-    public string PhoneNumber { get; set; }
-    public bool IsActive { get; set; }
-
-    public virtual ICollection<Comment> Comments { get; set; }
-    public virtual ICollection<Post> Posts { get; set; }
-  }
 }
