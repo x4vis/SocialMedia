0f4764355bcb78f4fe7c6589a34b087e5759b33c
Javier Soto <javislop@gmail.com>
2021-11-17 00:17:15 -0500

    add AddPost Dto and remove mapper method from Post DTO

--- SocialMedia.Core/DTOs/Command/AddPostDTO.cs
+++ SocialMedia.Core/DTOs/Command/AddPostDTO.cs
@@ -1,7 +1,12 @@
+using System;
+
 namespace SocialMedia.Core.DTOs.Command
 {
     public class AddPostDTO
     {
-        
+        public int UserId { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
     }
 }
--- SocialMedia.Core/DTOs/Query/PostDTO.cs
+++ SocialMedia.Core/DTOs/Query/PostDTO.cs
@@ -1,27 +1,13 @@
-using System.Net.Mime;
 using System;
-using SocialMedia.Core.Entities;
 
 namespace SocialMedia.Core.DTOs.Query
 {
-  public class PostDTO
-  {
-    public int PostId { get; set; }
-    public int UserId { get; set; }
-    public DateTime Date { get; set; }
-    public string Description { get; set; }
-    public string Image { get; set; }
-
-    public static PostDTO FromDataModel(Post entity)
+    public class PostDTO
     {
-      return entity == null ? null : new PostDTO
-      {
-        PostId = entity.PostId,
-        UserId = entity.UserId,
-        Date = entity.Date,
-        Description = entity.Description,
-        Image = entity.Image
-      };
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
     }
-  }
 }
