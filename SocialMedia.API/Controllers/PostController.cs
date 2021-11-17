89401496f0e0488cfd17f1a9813c6bf9a1712096
Javier Soto <javislop@gmail.com>
2021-11-17 00:12:15 -0500

    add DI post repositories, and add some validations

--- SocialMedia.API/Controllers/PostController.cs
+++ SocialMedia.API/Controllers/PostController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.DTOs.Command;
 using SocialMedia.Core.DTOs.Query;
 using SocialMedia.Core.Interfaces.Command;
 using SocialMedia.Core.Interfaces.Query;
@@ -11,48 +12,49 @@ namespace SocialMedia.API.Controllers
     [Route("api/[controller]")]
     public class PostController : ControllerBase
     {
-    private readonly IPostRepositoryQuery _repositoryQuery;
-    private readonly IPostRepositoryCommand _repositoryCommand;
+        private readonly IPostRepositoryQuery _postRepositoryQuery;
+        private readonly IPostRepositoryCommand _postRepositoryCommand;
 
-    public PostController(
-        IPostRepositoryQuery repositoryQuery,
-        IPostRepositoryCommand repositoryCommand
-    )
-    {
-        _repositoryQuery = repositoryQuery;
-        _repositoryCommand = repositoryCommand;
-    }
-
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<PostDTO>>> GetPosts()
-    {
-        var posts = await _repositoryQuery.GetPosts();
-        return Ok(posts);
-    }
-
-    [HttpGet("{id:int}")]
-    public async Task<ActionResult<PostDTO>> GetPost(int id)
-    {
-        var post = await _repositoryQuery.GetPost(id);
-
-        if (post == null)
+        public PostController
+        (
+            IPostRepositoryQuery postRepositoryquery,
+            IPostRepositoryCommand repositoryCommand
+        )
         {
-        return NotFound();
-        }
-        return Ok(post);
-    }
-
-    [HttpPost]
-    public async Task<ActionResult<PostDTO>> AddPost(PostDTO model)
-    {
-        var added = await _repositoryCommand.AddPost(model);
-
-        if (added == -1)
-        {
-        return StatusCode(500);
+            _postRepositoryQuery = postRepositoryquery;
+            _postRepositoryCommand = repositoryCommand;
         }
 
-        return Ok(model);
-    }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PostDTO>>> GetPosts()
+        {
+            var posts = await _postRepositoryQuery.GetPosts();
+            return Ok(posts);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<PostDTO>> GetPost(int id)
+        {
+            var post = await _postRepositoryQuery.GetPost(id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PostDTO>> AddPost(AddPostDTO model)
+        {
+            var added = await _postRepositoryCommand.AddPost(model);
+
+            if (added == -1)
+            {
+                return StatusCode(500);
+            }
+
+            return Ok();
+        }
     }
 }
