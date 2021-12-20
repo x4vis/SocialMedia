b1883a103b215a6b8699432197f14f40a1fbebbf
Javier Soto <javislop@gmail.com>
2021-12-19 21:50:51 -0500

    addd new implementations and change some logic for returns

--- SocialMedia.Infrastructure/Repositories/Command/PostRepositoryCommand.cs
+++ SocialMedia.Infrastructure/Repositories/Command/PostRepositoryCommand.cs
@@ -5,6 +5,7 @@ using SocialMedia.Core.Interfaces.Command;
 using SocialMedia.Infrastructure.Data.Context;
 using SocialMedia.Core.DTOs.Command;
 using SocialMedia.Infrastructure.Mappers.Command;
+using SocialMedia.Core.Enums;
 
 namespace SocialMedia.Infrastructure.Repositories.Commands
 {
@@ -17,23 +18,69 @@ namespace SocialMedia.Infrastructure.Repositories.Commands
             _context = context;
         }
 
-        public async Task<int> AddPost(AddPostDTO model)
+        public async Task<SaveResource> AddPostAsync(AddPostDTO model)
         {
-            var saved = 0;
-
             try
             {
                 var dataModel = AddPostMapper.ToDataModel(model);
                 _context.Add(dataModel);
-                await _context.SaveChangesAsync();
+                var rowsAffected = await _context.SaveChangesAsync();
+                return rowsAffected > 0 ? SaveResource.Saved : SaveResource.NotSaved;
             }
             catch (Exception ex)
             {
-                saved = -1;
                 Debug.WriteLine($"An error has ocurred while adding a post ==> {ex}");
+                return SaveResource.Error;
+            }
+        }
+
+        public async Task<UpdateOrDeleteResource> UpdatePostAsync(int postId, UpdatePostDTO model)
+        {
+            try
+            {
+                var currentPost = await _context.Posts.FindAsync(postId);
+
+                if (currentPost == null)
+                {
+                    return UpdateOrDeleteResource.NotFound;
+                }
+
+                currentPost.Date = model.Date;
+                currentPost.Description = model.Description;
+                currentPost.Image = model.Image;
+
+                int rowsAffected = await _context.SaveChangesAsync();
+                return rowsAffected > 0 ? UpdateOrDeleteResource.Written : UpdateOrDeleteResource.NotWritten;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"An error has ocurred while updating the post {ex}");
+                return UpdateOrDeleteResource.Error;
+            }
+        }
+
+        public async Task<UpdateOrDeleteResource> DeletePostAsync(int postId)
+        {
+            try
+            {
+                var currentPost = await _context.Posts.FindAsync(postId);
+
+                if (currentPost == null)
+                {
+                    return UpdateOrDeleteResource.NotFound;
+                }
+
+                _context.Posts.Remove(currentPost);
+
+                int rowsAffected = await _context.SaveChangesAsync();
+                return rowsAffected > 0 ? UpdateOrDeleteResource.Written : UpdateOrDeleteResource.NotWritten;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"An error has ocurred while updating the post {ex}");
+                return UpdateOrDeleteResource.Error;
             }
 
-            return saved;
         }
     }
 }
