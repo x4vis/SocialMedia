02b9ae138238e28d4d3daea60813883599861f78
Javier Soto <javislop@gmail.com>
2021-12-19 21:31:31 -0500

    delete ctor

--- SocialMedia.Core/DTOs/Command/UpdatePostDTO.cs
+++ SocialMedia.Core/DTOs/Command/UpdatePostDTO.cs
@@ -3,8 +3,8 @@ namespace SocialMedia.Core.DTOs.Command
 {
     public class UpdatePostDTO
     {
-        public UpdatePostDTO()
-        {
-        }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
     }
 }
