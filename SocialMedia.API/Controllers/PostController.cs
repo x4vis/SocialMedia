b42eb6f429d4bb7b7d424314cddafe60a7dfbed9
Javier Soto <javislop@gmail.com>
2021-12-19 21:29:20 -0500

    add new delete and update endpoints for post and change the logic to return a specific action result

--- SocialMedia.API/Controllers/PostController.cs
+++ SocialMedia.API/Controllers/PostController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SocialMedia.Core.DTOs.Command;
 using SocialMedia.Core.DTOs.Query;
+using SocialMedia.Core.Enums;
 using SocialMedia.Core.Interfaces.Command;
 using SocialMedia.Core.Interfaces.Query;
 
@@ -28,14 +29,14 @@ namespace SocialMedia.API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<PostDTO>>> GetPosts()
         {
-            var posts = await _postRepositoryQuery.GetPosts();
+            var posts = await _postRepositoryQuery.GetPostsAsync();
             return Ok(posts);
         }
 
         [HttpGet("{id:int}")]
         public async Task<ActionResult<PostDTO>> GetPost(int id)
         {
-            var post = await _postRepositoryQuery.GetPost(id);
+            var post = await _postRepositoryQuery.GetPostAsync(id);
 
             if (post == null)
             {
@@ -45,16 +46,55 @@ namespace SocialMedia.API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<PostDTO>> AddPost(AddPostDTO model)
+        public async Task<ActionResult> AddPost(AddPostDTO model)
         {
-            var added = await _postRepositoryCommand.AddPost(model);
+            var postAdded = await _postRepositoryCommand.AddPostAsync(model);
 
-            if (added == -1)
+            switch (postAdded)
             {
-                return StatusCode(500);
+                case SaveResource.NotSaved:
+                    return StatusCode(500, "The post could not be written in the DB, please try again");
+                case SaveResource.Error:
+                    return StatusCode(500, "The post could not be saved, please try again");
+                default:
+                    return NoContent();
             }
+        }
 
-            return Ok();
+        [HttpPut]
+        public async Task<IActionResult> UpdatePost(int postId, UpdatePostDTO model)
+        {
+            var postUpdated = await _postRepositoryCommand.UpdatePostAsync(postId, model);
+
+            switch (postUpdated)
+            {
+                case UpdateOrDeleteResource.NotFound:
+                    return NotFound();
+                case UpdateOrDeleteResource.NotWritten:
+                    return StatusCode(500, "The post could not be written in the DB, please try again");
+                case UpdateOrDeleteResource.Error:
+                    return StatusCode(500, "The post could not be updated, please try again");
+                default:
+                    return NoContent();
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeletePost(int id)
+        {
+            var postDeleted = await _postRepositoryCommand.DeletePostAsync(id);
+
+            switch (postDeleted)
+            {
+                case UpdateOrDeleteResource.NotFound:
+                    return NotFound();
+                case UpdateOrDeleteResource.NotWritten:
+                    return StatusCode(500, "The post could not be written in the DB, please try again");
+                case UpdateOrDeleteResource.Error:
+                    return StatusCode(500, "The post could not be deleted, please try again");
+                default:
+                    return Ok();
+            }
         }
     }
 }
