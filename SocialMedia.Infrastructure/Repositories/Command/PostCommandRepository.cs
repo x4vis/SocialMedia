3b89a5de50bd4002d954eba11245750c05347b13
Javier Soto <javislop@gmail.com>
2022-01-07 23:02:22 -0500

    change addPostAsync method to focus just on add the model to th DB

--- SocialMedia.Infrastructure/Repositories/Command/PostCommandRepository.cs
+++ SocialMedia.Infrastructure/Repositories/Command/PostCommandRepository.cs
@@ -4,34 +4,25 @@ using System.Threading.Tasks;
 using SocialMedia.Core.Interfaces.Command;
 using SocialMedia.Infrastructure.Data.Context;
 using SocialMedia.Core.DTOs.Command;
-using SocialMedia.Infrastructure.Mappers.Command;
 using SocialMedia.Core.Enums;
+using SocialMedia.Core.Entities;
 
 namespace SocialMedia.Infrastructure.Repositories.Commands
 {
-    public class PostRepositoryCommand : IPostRepositoryCommand
+    public class PostCommandRepository : IPostCommandRepository
     {
         private readonly SocialMediaContext _context;
 
-        public PostRepositoryCommand(SocialMediaContext context)
+        public PostCommandRepository(SocialMediaContext context)
         {
             _context = context;
         }
 
-        public async Task<SaveResource> AddPostAsync(AddPostDTO model)
+        public async Task<int> AddPostAsync(Post model)
         {
-            try
-            {
-                var dataModel = AddPostMapper.ToDataModel(model);
-                _context.Add(dataModel);
-                var rowsAffected = await _context.SaveChangesAsync();
-                return rowsAffected > 0 ? SaveResource.Saved : SaveResource.NotSaved;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"An error has ocurred while adding a post ==> {ex}");
-                return SaveResource.Error;
-            }
+            _context.Add(model);
+            var rowsAffected = await _context.SaveChangesAsync();
+            return rowsAffected;
         }
 
         public async Task<UpdateOrDeleteResource> UpdatePostAsync(int postId, UpdatePostDTO model)
