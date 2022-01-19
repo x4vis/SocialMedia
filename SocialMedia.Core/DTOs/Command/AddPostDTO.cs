0188b8401d2d44681220ea060f41775e9962aa1d
Javier Soto <javislop@gmail.com>
2022-01-19 14:54:48 -0500

    remove prop userid to use inheritance from baseEntity

--- SocialMedia.Core/DTOs/Command/AddPostDTO.cs
+++ SocialMedia.Core/DTOs/Command/AddPostDTO.cs
@@ -1,10 +1,10 @@
 using System;
+using SocialMedia.Core.Entities;
 
 namespace SocialMedia.Core.DTOs.Command
 {
-    public class AddPostDTO
+    public class AddPostDTO : BaseEntity
     {
-        public int UserId { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
