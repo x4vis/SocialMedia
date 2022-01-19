5b6b52a83371181a28cf8b99cbf3732e6ce45405
Javier Soto <javislop@gmail.com>
2022-01-19 15:24:50 -0500

    add command an query base repositories and remove unnecessary classes

---
+++ SocialMedia.Infrastructure/Repositories/Command/BaseCommandRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SocialMedia.Core.Entities;
+using SocialMedia.Core.Enums;
+using SocialMedia.Core.Interfaces.Command;
+using SocialMedia.Infrastructure.Data.Context;
+
+namespace SocialMedia.Infrastructure.Repositories.Command
+{
+    public class BaseCommandRepository<T> : IBaseCommandRepository<T> where T : BaseEntity
+    {
+        private readonly SocialMediaContext _context;
+        private readonly DbSet<T> _entity;
+
+        public BaseCommandRepository(SocialMediaContext context)
+        {
+            _context = context;
+            _entity = context.Set<T>();
+        }
+
+        public async Task<WriteEntity> AddAsync(T entity)
+        {
+            try
+            {
+                _context.Add(entity);
+                var rowsAffected = await _context.SaveChangesAsync();
+                return rowsAffected > 0 ? WriteEntity.Written : WriteEntity.NotWritten;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"An error has ocurred while adding the entity ==> {ex}");
+                return WriteEntity.Error;
+            }
+        }
+
+        public async Task<WriteEntity> UpdateAsync(T entity)
+        {
+            try
+            {
+                _entity.Update(entity);
+                var rowsAffected = await _context.SaveChangesAsync();
+                return rowsAffected > 0 ? WriteEntity.Written : WriteEntity.NotWritten;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"An error has ocurred while updating the entity {ex}");
+                return WriteEntity.Error;
+            }
+        }
+
+        public async Task<WriteEntity> DeleteAsync(T entity)
+        {
+            try
+            {
+                _entity.Remove(entity);
+                int rowsAffected = await _context.SaveChangesAsync();
+                return rowsAffected > 0 ? WriteEntity.Written : WriteEntity.NotWritten;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"An error has ocurred while updating the entity {ex}");
+                return WriteEntity.Error;
+            }
+
+        }
+
+    }
+}
---
+++ SocialMedia.Infrastructure/Repositories/Query/BaseQueryRepository.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces.Query;
+using SocialMedia.Infrastructure.Data.Context;
+
+namespace SocialMedia.Infrastructure.Repositories.Query
+{
+    public class BaseQueryRepository<T> : IBaseQueryRepository<T> where T : BaseEntity
+    {
+        private readonly DbSet<T> _entity;
+
+        public BaseQueryRepository(SocialMediaContext context)
+        {
+            _entity = context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(bool isTracked)
+        {
+            if (isTracked)
+            {
+                return await _entity.AsNoTracking().ToListAsync();
+            }
+
+            return await _entity.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id, bool isTracked)
+        {
+            if (isTracked)
+            {
+                return await _entity
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(x => x.Id == id);
+            }
+
+            return await _entity.FindAsync(id);
+        }
+    }
+}
