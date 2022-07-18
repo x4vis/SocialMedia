70abe596f9ba0c5a543a88c98b41a445a60714d8
Javier Soto <javislop@gmail.com>
2022-07-17 22:17:18 -0400

    renombra y cambia las propiedades para la respuesta generica del API

---
+++ SocialMedia.Core/DTOs/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace SocialMedia.Core.DTOs
+{
+    public class Response<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T Data { get; set; }
+        public string Message { get; set; }
+    }
+}
