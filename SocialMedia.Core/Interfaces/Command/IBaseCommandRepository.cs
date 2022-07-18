efc976b70c1f803fdd34f38cc878fc5e77b1bd57
Javier Soto <javislop@gmail.com>
2022-07-17 22:19:33 -0400

    remove write entity enum and leave the repository out of the try catch block

--- SocialMedia.Core/Interfaces/Command/IBaseCommandRepository.cs
+++ SocialMedia.Core/Interfaces/Command/IBaseCommandRepository.cs
@@ -1,13 +1,12 @@
 ﻿using System.Threading.Tasks;
 using SocialMedia.Core.Entities;
-using SocialMedia.Core.Enums;
 
 namespace SocialMedia.Core.Interfaces.Command
 {
     public interface IBaseCommandRepository<T> where T : BaseEntity
     {
-        Task<WriteEntity> AddAsync(T entity);
-        Task<WriteEntity> UpdateAsync(T entity);
-        Task<WriteEntity> DeleteAsync(T entity);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
     }
 }
--- SocialMedia.Infrastructure/Repositories/Command/BaseCommandRepository.cs
+++ SocialMedia.Infrastructure/Repositories/Command/BaseCommandRepository.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SocialMedia.Core.Entities;
-using SocialMedia.Core.Enums;
 using SocialMedia.Core.Interfaces.Command;
 using SocialMedia.Infrastructure.Data.Context;
 
@@ -20,50 +19,22 @@ namespace SocialMedia.Infrastructure.Repositories.Command
             _entity = context.Set<T>();
         }
 
-        public async Task<WriteEntity> AddAsync(T entity)
+        public async Task AddAsync(T entity)
         {
-            try
-            {
-                _context.Add(entity);
-                var rowsAffected = await _context.SaveChangesAsync();
-                return rowsAffected > 0 ? WriteEntity.Written : WriteEntity.NotWritten;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"An error has ocurred while adding the entity ==> {ex}");
-                return WriteEntity.Error;
-            }
+            _context.Add(entity);
+            await _context.SaveChangesAsync();
         }
 
-        public async Task<WriteEntity> UpdateAsync(T entity)
+        public async Task UpdateAsync(T entity)
         {
-            try
-            {
-                _entity.Update(entity);
-                var rowsAffected = await _context.SaveChangesAsync();
-                return rowsAffected > 0 ? WriteEntity.Written : WriteEntity.NotWritten;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"An error has ocurred while updating the entity {ex}");
-                return WriteEntity.Error;
-            }
+            _entity.Update(entity);
+            await _context.SaveChangesAsync();
         }
 
-        public async Task<WriteEntity> DeleteAsync(T entity)
+        public async Task DeleteAsync(T entity)
         {
-            try
-            {
-                _entity.Remove(entity);
-                int rowsAffected = await _context.SaveChangesAsync();
-                return rowsAffected > 0 ? WriteEntity.Written : WriteEntity.NotWritten;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"An error has ocurred while updating the entity {ex}");
-                return WriteEntity.Error;
-            }
-
+            _entity.Remove(entity);
+            await _context.SaveChangesAsync();
         }
 
     }
